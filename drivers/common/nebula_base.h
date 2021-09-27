71e767d0cce0653338f299101a33744422b48d0e
Abraham Cano <abrahammonrroy@yahoo.com>
2021-09-27 22:46:07 +0900

    nebula structure
    Signed-off-by: Abraham Cano <abrahammonrroy@yahoo.com>

---
+++ drivers/common/nebula_base.h
@@ -0,0 +1,12 @@
+#ifndef NEBULA_DRIVER_NEBULA_BASE_H
+#define NEBULA_DRIVER_NEBULA_BASE_H
+
+namespace nebula {
+namespace drivers{
+    class NebulaBaseDriver
+    {
+
+    };
+}
+}
+#endif //NEBULA_DRIVER_NEBULA_BASE_H
