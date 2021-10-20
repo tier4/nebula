4a086a98fd77e30ffd2fac7a987a5e7106be38c8
amc-nu <abrahammonrroy@yahoo.com>
2021-10-20 12:06:33 +0900

    nebula wip
    Signed-off-by: amc-nu <abrahammonrroy@yahoo.com>

--- drivers/common/nebula_base.h
+++ drivers/common/nebula_base.h
@@ -1,12 +1,38 @@
 #ifndef NEBULA_DRIVER_NEBULA_BASE_H
 #define NEBULA_DRIVER_NEBULA_BASE_H
 
-namespace nebula {
-namespace drivers{
-    class NebulaBaseDriver
-    {
+namespace nebula
+{
+namespace drivers
+{
+class NebulaBaseDriver
+{
+  NebulaDriverBase(const NebulaDriverBase&) = delete;
+  NebulaDriverBase & operator=(const NebulaDriverBase&) = delete;
 
-    };
-}
-}
-#endif //NEBULA_DRIVER_NEBULA_BASE_H
+  public:
+  virtual ~NebulaDriverBase() {}
+
+  virtual bool SetConfiguration() = 0;
+  virtual bool GetConfiguration() const = 0;
+
+  virtual void SetCalibration() = 0;
+  virtual void GetCalibration() = 0;
+
+  virtual void StartHwRxInterface() = 0;
+  virtual void StopHwRxInterface() = 0;
+
+  virtual int ParsePacket() = 0;
+  virtual void* GenerateCloud() = 0;
+
+  virtual bool Initialize();
+  virtual LidarStatus GetLidarStatus();
+
+  virtual void SetPublishLidarDataCallback(void*);
+  virtual DiagnosticsStatus GetDiagnosticsStatus();
+
+};
+
+}  // namespace drivers
+}  // namespace nebula
+#endif  //NEBULA_DRIVER_NEBULA_BASE_H
