32b2b564457bcaefcd8f51af4f563e152a9ed186
Jacob Lambert <jacob.lambert@tier4.jp>
2021-11-18 18:02:51 +0900

    start of packet handling

--- nebula_drivers/src/hesai/hesai_hw_interface.cpp
+++ nebula_drivers/src/hesai/hesai_hw_interface.cpp
@@ -10,51 +10,49 @@ HesaiHwInterface::HesaiHwInterface()
 {
 }
 
-HesaiHwInterface::HesaiHwInterface(
-  std::shared_ptr<SensorConfigurationBase> & sensor_configuration,
-  CalibrationConfigurationBase & calibration_configuration)
-: cloud_io_context_{new IoContext(1)},
-  cloud_udp_driver_{new ::drivers::udp_driver::UdpDriver(*cloud_io_context_)}
-{
-}
-
 Status HesaiHwInterface::SetSensorConfiguration(
   std::shared_ptr<SensorConfigurationBase> sensor_configuration)
 {
   try {
     sensor_configuration_ =
       std::static_pointer_cast<HesaiSensorConfiguration>(sensor_configuration);
-    return Status::OK;
   } catch (const std::exception & ex) {
     Status status = Status::SENSOR_CONFIG_ERROR;
     std::cerr << NebulaStatusToString(status) << std::endl;
     return status;
   }
+  return Status::OK;
 }
 
 Status HesaiHwInterface::CloudInterfaceStart()
 {
   try {
-    std::cout << "Starting UDP server on: " << sensor_configuration_->sensor_ip << "."
+    std::cout << "Starting UDP server on: " << sensor_configuration_->host_ip << "."
               << sensor_configuration_->data_port << std::endl;
     cloud_udp_driver_->init_receiver(
-      sensor_configuration_->sensor_ip, sensor_configuration_->data_port);
+      sensor_configuration_->host_ip, sensor_configuration_->data_port);
     cloud_udp_driver_->receiver()->open();
     cloud_udp_driver_->receiver()->bind();
     cloud_udp_driver_->receiver()->asyncReceive(
       std::bind(&HesaiHwInterface::ReceiveCloudPacketCallback, this, std::placeholders::_1));
-    std::cout << "After callback" << std::endl;
   } catch (const std::exception & ex) {
     Status status = Status::UDP_CONNECTION_ERROR;
     std::cerr << NebulaStatusToString(status) << sensor_configuration_->sensor_ip << ","
               << sensor_configuration_->data_port << std::endl;
-  }
+    return status;
+    }
   return Status::OK;
 }
 
 void HesaiHwInterface::ReceiveCloudPacketCallback(const std::vector<uint8_t> & buffer)
 {
-  std::cout << "Got a Packet. Form the scan here" << std::endl;
+  std::cout << "New packet of size:" << buffer.size() << std::endl;
+  size_t packet_size = buffer.size();
+  if (packet_size == 1194 || packet_size == 1198) {     // todo: this is for pandar, implement universal _is_valid function
+    cur_pkt_ = buffer;
+    // send one level higher and publish
+  }
+
 }
 Status HesaiHwInterface::CloudInterfaceStop()
 {
--- nebula_drivers/src/hesai/hesai_hw_interface_ros_wrapper.cpp
+++ nebula_drivers/src/hesai/hesai_hw_interface_ros_wrapper.cpp
@@ -8,29 +8,62 @@ HesaiHwInterfaceWrapper::HesaiHwInterfaceWrapper(
   const rclcpp::NodeOptions & options, const std::string & node_name)
 : rclcpp::Node(node_name, options), hw_interface_()
 {
+  // Get these params form YAML or XML file
+  sensor_model_ = "Pandar64";
+  host_ip_ = "255.255.255.255";
+  sensor_ip_ = "192.168.1.201";
+  data_port_ = 2368;
+  gnss_port_ = 0;
+  frequency_ms_ = 100;
+  echo_mode_ = "Dual";
+
+  // Build sensor_configuration file
+  sensor_configuration_.sensor_ip = sensor_ip_;
+  sensor_configuration_.host_ip = host_ip_;
+  sensor_configuration_.data_port = data_port_;
+  sensor_configuration_.gnss_port = gnss_port_;
+  sensor_configuration_.frequency_ms = frequency_ms_;   // todo: this is weird, frequency_hz is better maybe?
+  // Echo mode and model will need some type of switch/case or if/else
+  sensor_configuration_.echo_mode = drivers::EchoMode::SINGLE_STRONGEST;
+  sensor_configuration_.sensor_model = drivers::SensorModel::PANDAR64;
+
+  // Initialize sensor_configuration
+  std::shared_ptr<drivers::SensorConfigurationBase> sensor_cfg_ptr =
+    std::make_shared<drivers::HesaiSensorConfiguration>(sensor_configuration_);
+  std::static_pointer_cast<drivers::SensorConfigurationBase>(sensor_cfg_ptr);
+  hw_interface_.SetSensorConfiguration(
+    std::static_pointer_cast<drivers::SensorConfigurationBase>(sensor_cfg_ptr));
 }
 
 Status HesaiHwInterfaceWrapper::StreamStart() {
-  drivers::HesaiSensorConfiguration sensor_configuration;
-  sensor_configuration.sensor_ip = "255.255.255.255";
-  sensor_configuration.data_port = 2370;
-  sensor_configuration.gnss_port = 0;
-
-  std::shared_ptr<drivers::SensorConfigurationBase> sensor_cfg_ptr =
-    std::make_shared<drivers::HesaiSensorConfiguration>(sensor_configuration);
-  hw_interface_.SetSensorConfiguration(
-    std::static_pointer_cast<drivers::SensorConfigurationBase>(sensor_cfg_ptr));
-  hw_interface_.CloudInterfaceStart();
+hw_interface_.CloudInterfaceStart();
   return Status::OK; }
 
 Status HesaiHwInterfaceWrapper::StreamStop() { return Status::OK; }
 Status HesaiHwInterfaceWrapper::Shutdown() { return Status::OK; }
 
-Status HesaiHwInterfaceWrapper::InitializeHwInterface(
-  const drivers::SensorConfigurationBase & sensor_configuration)
-{
+Status HesaiHwInterfaceWrapper::InitializeHwInterface(      // todo: don't think this is needed
+  const drivers::SensorConfigurationBase & sensor_configuration) {
+  return Status::OK;
 }
-void HesaiHwInterfaceWrapper::ReceiveDataPacketCallback(const std::vector<uint8_t> & buffer) {}
+
+void HesaiHwInterfaceWrapper::ReceiveDataPacketCallback(const std::vector<uint8_t> & buffer) {
+  // Todo: how do I wrap this in a callback for cur_pkt_?
+  // todo: how to bring msgs in hesai_hw_interface_ros_wrapper.hpp?
+
+  // Create new PandarPacket
+  // Add current packet, fix header
+  // Publish
+
+  // Check if beginning of scan (somehow must be based on azimuth angle? or total size, but I still
+  // to check and start at the right place...)
+    // Create new hesai_msgs::PandarScan
+
+  // Add packet to scan
+
+  // Check if scan is complete
+    // Publish
+}
 
 }  // namespace ros
 }  // namespace nebula
