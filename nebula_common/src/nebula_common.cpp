4d5e5a58d14464115933bc83523616cf6f95f97f
amc-nu <abraham.monrroy@gmail.com>
2023-06-07 12:30:32 +0900

    nebula_common. add aw point type conversions
    Signed-off-by: amc-nu <abraham.monrroy@gmail.com>

--- nebula_common/src/nebula_common.cpp
+++ nebula_common/src/nebula_common.cpp
@@ -36,6 +36,31 @@ pcl::PointCloud<PointXYZIR>::Ptr convertPointXYZIRCAEDTToPointXYZIR(
     point.y = p.y;
     point.z = p.z;
     point.intensity = p.intensity;
+    point.ring = p.channel;
+    output_pointcloud->points.emplace_back(point);
+  }
+
+  output_pointcloud->header = input_pointcloud->header;
+  output_pointcloud->height = 1;
+  output_pointcloud->width = output_pointcloud->points.size();
+  return output_pointcloud;
+}
+
+pcl::PointCloud<PointXYZIRADT>::Ptr convertPointXYZIRCAEDTToPointXYZIRADT(
+  const pcl::PointCloud<PointXYZIRCAEDT>::ConstPtr & input_pointcloud)
+{
+  pcl::PointCloud<PointXYZIRADT>::Ptr output_pointcloud(new pcl::PointCloud<PointXYZIRADT>);
+  output_pointcloud->reserve(input_pointcloud->points.size());
+  PointXYZIRADT point;
+  for (const auto & p : input_pointcloud->points) {
+    point.x = p.x;
+    point.y = p.y;
+    point.z = p.z;
+    point.intensity = p.intensity;
+    point.ring = p.channel;
+    point.azimuth = p.azimuth;
+    point.distance = p.distance;
+    point.time_stamp = p.time_stamp;
     output_pointcloud->points.emplace_back(point);
   }
 
