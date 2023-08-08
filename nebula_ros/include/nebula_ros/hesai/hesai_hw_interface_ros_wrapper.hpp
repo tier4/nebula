0c40a1d87b05e02b318c8babadce579919502339
Abraham Monrroy Cano <abrahammonrroy@yahoo.com>
2023-08-08 13:35:04 +0900

    refactor: rename transport_drivers to boost_transport_driver (#46)
    * rename transport_drivers to boost_transport_driver
    
    Signed-off-by: amc-nu <abraham.monrroy@gmail.com>
    
    * cspell. fix
    
    Signed-off-by: amc-nu <abraham.monrroy@gmail.com>
    
    ---------
    
    Signed-off-by: amc-nu <abraham.monrroy@gmail.com>

--- nebula_ros/include/nebula_ros/hesai/hesai_hw_interface_ros_wrapper.hpp
+++ nebula_ros/include/nebula_ros/hesai/hesai_hw_interface_ros_wrapper.hpp
@@ -5,7 +5,7 @@
 #include "nebula_common/nebula_common.hpp"
 #include "nebula_hw_interfaces/nebula_hw_interfaces_hesai/hesai_hw_interface.hpp"
 #include "nebula_ros/common/nebula_hw_interface_ros_wrapper_base.hpp"
-#include "tcp_driver/tcp_driver.hpp"
+#include "boost_tcp_driver/tcp_driver.hpp"
 
 #include <ament_index_cpp/get_package_prefix.hpp>
 #include <rclcpp/rclcpp.hpp>
--- nebula_ros/include/nebula_ros/hesai/hesai_hw_monitor_ros_wrapper.hpp
+++ nebula_ros/include/nebula_ros/hesai/hesai_hw_monitor_ros_wrapper.hpp
@@ -5,7 +5,7 @@
 #include "nebula_common/nebula_common.hpp"
 #include "nebula_hw_interfaces/nebula_hw_interfaces_hesai/hesai_hw_interface.hpp"
 #include "nebula_ros/common/nebula_hw_monitor_ros_wrapper_base.hpp"
-#include "tcp_driver/tcp_driver.hpp"
+#include "boost_tcp_driver/tcp_driver.hpp"
 
 #include <ament_index_cpp/get_package_prefix.hpp>
 #include <diagnostic_updater/diagnostic_updater.hpp>
@@ -15,6 +15,11 @@
 #include <boost/asio.hpp>
 
 #include <mutex>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <thread>
 
 namespace nebula
 {
