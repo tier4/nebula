39e10c684585ce5aa8d3cdf72a7b1fdba713b795
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2023-08-10 13:49:49 +0300

    Check invalid points by their ring and angle and do not use them

--- nebula_decoders/src/nebula_decoders_velodyne/decoders/vlp16_decoder.cpp
+++ nebula_decoders/src/nebula_decoders_velodyne/decoders/vlp16_decoder.cpp
@@ -315,6 +315,9 @@ void Vlp16Decoder::unpack(const velodyne_msgs::msg::VelodynePacket & velodyne_pa
                 current_point.return_type = return_type;
                 current_point.channel = corrections.laser_ring;
                 current_point.azimuth = rotation_radians_[azimuth_corrected];
+
+                if (check_invalid_point(corrections.laser_ring, azimuth_corrected)) continue;
+
                 current_point.elevation = sin_vert_angle;
                 auto point_ts = block_timestamp - scan_timestamp_ + point_time_offset;
                 if (point_ts < 0) point_ts = 0;
