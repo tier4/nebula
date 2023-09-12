743477fee94b8bff1a94f461a533e2b3080d2376
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2023-09-12 17:08:30 +0300

    Created Robosense hardware interface

---
+++ nebula_hw_interfaces/include/nebula_hw_interfaces/nebula_hw_interfaces_robosense/robosense_hw_interface.hpp
@@ -0,0 +1,102 @@
+#ifndef NEBULA_ROBOSENSE_HW_INTERFACE_H
+#define NEBULA_ROBOSENSE_HW_INTERFACE_H
+
+// Have to define macros to silence warnings about deprecated headers being used by
+// boost/property_tree/ in some versions of boost.
+// See: https://github.com/boostorg/property_tree/issues/51
+#include <boost/version.hpp>
+#if (BOOST_VERSION / 100 >= 1073 && BOOST_VERSION / 100 <= 1076)  // Boost 1.73 - 1.76
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
+#endif
+#if (BOOST_VERSION / 100 == 1074)  // Boost 1.74
+#define BOOST_ALLOW_DEPRECATED_HEADERS
+#endif
+
+#include "boost_udp_driver/udp_driver.hpp"
+#include "nebula_common/robosense/robosense_common.hpp"
+#include "nebula_hw_interfaces/nebula_hw_interfaces_common/nebula_hw_interface_base.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "pandar_msgs/msg/pandar_packet.hpp"
+#include "pandar_msgs/msg/pandar_scan.hpp"
+
+namespace nebula
+{
+namespace drivers
+{
+
+const uint16_t MTU_SIZE = 1500;
+const uint16_t HELIOS5515_PACKET_SIZE = 1248;
+const uint16_t BPEARL_PACKET_SIZE = 1248;
+
+/// @brief Hardware interface of Robosense driver
+class RobosenseHwInterface : NebulaHwInterfaceBase
+{
+private:
+  std::unique_ptr<::drivers::common::IoContext> cloud_io_context_;
+  std::unique_ptr<::drivers::udp_driver::UdpDriver> cloud_udp_driver_;
+  std::shared_ptr<RobosenseSensorConfiguration> sensor_configuration_;
+  std::unique_ptr<pandar_msgs::msg::PandarScan> scan_cloud_ptr_;
+  size_t azimuth_index_{44};  // For Helios and Bpearl 42 byte header + 2 byte flag
+  int prev_phase_{};
+  std::function<bool(size_t)>
+    is_valid_packet_; /*Lambda Function Array to verify proper packet size*/
+  std::function<void(std::unique_ptr<pandar_msgs::msg::PandarScan> buffer)>
+    scan_reception_callback_; /**This function pointer is called when the scan is complete*/
+  std::shared_ptr<rclcpp::Logger> parent_node_logger_;
+
+  /// @brief Printing the string to RCLCPP_INFO_STREAM
+  /// @param info Target string
+  void PrintInfo(std::string info);
+
+  /// @brief Printing the string to RCLCPP_DEBUG_STREAM
+  /// @param debug Target string
+  void PrintDebug(std::string debug);
+
+public:
+  /// @brief Constructor
+  RobosenseHwInterface();
+
+  /// @brief Callback function to receive the Cloud Packet data from the UDP Driver
+  /// @param buffer Buffer containing the data received from the UDP socket
+  void ReceiveCloudPacketCallback(const std::vector<uint8_t> & buffer) final;
+
+  /// @brief Starting the interface that handles UDP streams
+  /// @return Resulting status
+  Status CloudInterfaceStart() final;
+
+  /// @brief Function for stopping the interface that handles UDP streams
+  /// @return Resulting status
+  Status CloudInterfaceStop() final;
+
+  /// @brief Setting sensor configuration
+  /// @param sensor_configuration SensorConfiguration for this interface
+  /// @return Resulting status
+  Status SetSensorConfiguration(
+    std::shared_ptr<SensorConfigurationBase> sensor_configuration) final;
+
+  /// @brief Printing sensor configuration
+  /// @param sensor_configuration SensorConfiguration for this interface
+  /// @return Resulting status
+  Status GetSensorConfiguration(SensorConfigurationBase & sensor_configuration) final;
+
+  /// @brief Printing calibration configuration
+  /// @param calibration_configuration CalibrationConfiguration for the checking
+  /// @return Resulting status
+  Status GetCalibrationConfiguration(CalibrationConfigurationBase & calibration_configuration);
+
+  /// @brief Registering callback for PandarScan
+  /// @param scan_callback Callback function
+  /// @return Resulting status
+  Status RegisterScanCallback(
+    std::function<void(std::unique_ptr<pandar_msgs::msg::PandarScan>)> scan_callback);
+
+  /// @brief Setting rclcpp::Logger
+  /// @param node Logger
+  void SetLogger(std::shared_ptr<rclcpp::Logger> logger);
+};
+
+}  // namespace drivers
+}  // namespace nebula
+#endif  // NEBULA_ROBOSENSE_HW_INTERFACE_H
---
+++ nebula_hw_interfaces/src/nebula_robosense_hw_interfaces/robosense_hw_interface.cpp
@@ -0,0 +1,143 @@
+#include "nebula_hw_interfaces/nebula_hw_interfaces_robosense/robosense_hw_interface.hpp"
+namespace nebula
+{
+namespace drivers
+{
+RobosenseHwInterface::RobosenseHwInterface()
+: cloud_io_context_{new ::drivers::common::IoContext(1)},
+  cloud_udp_driver_{new ::drivers::udp_driver::UdpDriver(*cloud_io_context_)},
+  scan_cloud_ptr_{std::make_unique<pandar_msgs::msg::PandarScan>()}
+{
+}
+
+void RobosenseHwInterface::ReceiveCloudPacketCallback(const std::vector<uint8_t> & buffer)
+{
+  int scan_phase = static_cast<int>(sensor_configuration_->scan_phase * 100.0);
+  if (!is_valid_packet_(buffer.size())) {
+    PrintDebug("Invalid Packet: " + std::to_string(buffer.size()));
+    return;
+  }
+  uint32_t buffer_size = buffer.size();
+  std::array<uint8_t, MTU_SIZE> packet_data{};
+  std::copy_n(std::make_move_iterator(buffer.begin()), buffer_size, packet_data.begin());
+  pandar_msgs::msg::PandarPacket pandar_packet;
+  pandar_packet.data = packet_data;
+  pandar_packet.size = buffer_size;
+  auto now = std::chrono::system_clock::now();
+  auto now_secs = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
+  auto now_nanosecs =
+    std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
+  pandar_packet.stamp.sec = static_cast<int>(now_secs);
+  pandar_packet.stamp.nanosec =
+    static_cast<int>((now_nanosecs / 1000000000. - static_cast<double>(now_secs)) * 1000000000);
+  scan_cloud_ptr_->packets.emplace_back(pandar_packet);
+
+  int current_phase = 0;
+  bool comp_flg = false;
+
+  const auto & data = scan_cloud_ptr_->packets.back().data;
+  current_phase = (data[azimuth_index_ + 1] & 0xff) + ((data[azimuth_index_] & 0xff) << 8);
+
+  //  PrintInfo("Current phase: " + std::to_string(current_phase));  ///////
+
+  current_phase = (static_cast<int>(current_phase) + 36000 - scan_phase) % 36000;
+
+  if (current_phase >= prev_phase_ || scan_cloud_ptr_->packets.size() < 2) {
+    prev_phase_ = current_phase;
+  } else {
+    comp_flg = true;
+  }
+
+  if (comp_flg) {  // Scan complete
+    if (scan_reception_callback_) {
+      scan_cloud_ptr_->header.stamp = scan_cloud_ptr_->packets.front().stamp;
+      // Callback
+      scan_reception_callback_(std::move(scan_cloud_ptr_));
+      scan_cloud_ptr_ = std::make_unique<pandar_msgs::msg::PandarScan>();
+    }
+  }
+}
+
+Status RobosenseHwInterface::CloudInterfaceStart()
+{
+  try {
+    std::cout << "Starting UDP server on: " << *sensor_configuration_ << std::endl;
+    cloud_udp_driver_->init_receiver(
+      sensor_configuration_->host_ip, sensor_configuration_->data_port);
+    cloud_udp_driver_->receiver()->open();
+    cloud_udp_driver_->receiver()->bind();
+
+    cloud_udp_driver_->receiver()->asyncReceive(
+      std::bind(&RobosenseHwInterface::ReceiveCloudPacketCallback, this, std::placeholders::_1));
+  } catch (const std::exception & ex) {
+    Status status = Status::UDP_CONNECTION_ERROR;
+    std::cerr << status << sensor_configuration_->sensor_ip << ","
+              << sensor_configuration_->data_port << std::endl;
+    return status;
+  }
+  return Status::OK;
+}
+
+Status RobosenseHwInterface::CloudInterfaceStop()
+{
+  return Status::ERROR_1;
+}
+
+Status RobosenseHwInterface::SetSensorConfiguration(
+  std::shared_ptr<SensorConfigurationBase> sensor_configuration)
+{
+  sensor_configuration_ =
+    std::static_pointer_cast<RobosenseSensorConfiguration>(sensor_configuration);
+
+  is_valid_packet_ = [](size_t packet_size) { return (packet_size == HELIOS5515_PACKET_SIZE); };
+
+  return Status::OK;
+}
+
+Status RobosenseHwInterface::GetSensorConfiguration(SensorConfigurationBase & sensor_configuration)
+{
+  std::stringstream ss;
+  ss << sensor_configuration;
+  PrintDebug(ss.str());
+  return Status::ERROR_1;
+}
+
+Status RobosenseHwInterface::GetCalibrationConfiguration(
+  CalibrationConfigurationBase & calibration_configuration)
+{
+  PrintDebug(calibration_configuration.calibration_file);
+  return Status::ERROR_1;
+}
+
+Status RobosenseHwInterface::RegisterScanCallback(
+  std::function<void(std::unique_ptr<pandar_msgs::msg::PandarScan>)> scan_callback)
+{
+  scan_reception_callback_ = std::move(scan_callback);
+  return Status::OK;
+}
+
+void RobosenseHwInterface::PrintDebug(std::string debug)
+{
+  if (parent_node_logger_) {
+    RCLCPP_DEBUG_STREAM((*parent_node_logger_), debug);
+  } else {
+    std::cout << debug << std::endl;
+  }
+}
+
+void RobosenseHwInterface::PrintInfo(std::string info)
+{
+  if (parent_node_logger_) {
+    RCLCPP_INFO_STREAM((*parent_node_logger_), info);
+  } else {
+    std::cout << info << std::endl;
+  }
+}
+
+void RobosenseHwInterface::SetLogger(std::shared_ptr<rclcpp::Logger> logger)
+{
+  parent_node_logger_ = logger;
+}
+
+}  // namespace drivers
+}  // namespace nebula
