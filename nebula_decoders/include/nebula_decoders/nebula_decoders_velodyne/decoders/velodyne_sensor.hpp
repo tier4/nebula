6dc9df5125b3e6194f3ae02d8b274fbef30d7988
Benjamin Gilby <gilbybenjamin@gmail.com>
2024-04-04 15:49:43 +0900

    save progress (velodyne_generic_decoder.cpp logic done except unpack())

---
+++ nebula_decoders/include/nebula_decoders/nebula_decoders_velodyne/decoders/velodyne_sensor.hpp
@@ -0,0 +1,40 @@
+#pragma once  // why this
+
+// NOTES:
+// how does "compute timing offsets" part of decoder work?
+
+namespace nebula
+{
+namespace drivers
+{
+
+class VLS128
+{
+public:
+  double full_firing_cycle_s;
+  double single_firing_s;
+  int scans_per_packet;
+
+  int blocks_per_packet;
+  int scan_per_blocks;
+
+  // int firings_per_block;
+
+  int channels_per_firing_sequence;
+
+  int num_channels;
+  int num_simultaneous_firings;
+
+  // only vls128, others should be 0
+  double offset_packet_time;
+
+  // only vlp16
+  int firings_per_scan;
+
+private:
+};
+}  // namespace drivers
+}  // namespace nebula
+
+// TODO: VLS128 only supports single return mode, yes? Should be error checking if dual_return is
+// set
