bdec15dcc3d435b5b6bb847c8e15fa38cb1287e9
Max SCHMELLER <max.schmeller@tier4.jp>
2024-04-05 17:22:12 +0900

    feat(nebula_common): more instrumentation tools

--- nebula_common/include/nebula_common/util/topic_delay.hpp
+++ nebula_common/include/nebula_common/util/topic_delay.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <builtin_interfaces/msg/time.hpp>
+
 #include "nebula_common/util/performance_counter.hpp"
+
 
 #include <chrono>
 #include <cstdint>
@@ -13,57 +16,38 @@ namespace util
 
 using namespace std::chrono_literals;
 
-class Instrumentation
+class TopicDelay
 {
   using clock = std::chrono::high_resolution_clock;
   using time_point = clock::time_point;
   using duration = std::chrono::nanoseconds;
 
 public:
-  Instrumentation(std::string tag)
-  : tag_(std::move(tag)), last_print_(clock::now()), delays_(), rtts_()
-  {
-  }
+  TopicDelay(std::string tag) : tag_(std::move(tag)), last_print_(clock::now()), delays_() {}
 
-  void tick()
+  void tick(builtin_interfaces::msg::Time & stamp)
   {
     auto now = clock::now();
 
-    if (last_tick_ != time_point::min()) {
-      auto rtt = now - last_tick_;
-      rtts_.update(rtt);
-    }
+    time_point t_msg = time_point(duration(static_cast<uint64_t>(stamp.sec) * 1'000'000'000ULL + stamp.nanosec));
+    auto delay = now - t_msg;
 
-    last_tick_ = now;
-  }
-
-  void tock()
-  {
-    auto now = clock::now();
-    auto delay = now - last_tick_;
     delays_.update(delay);
 
     if (now - last_print_ >= PRINT_INTERVAL) {
       auto del = delays_.reset();
-      auto rtt = rtts_.reset();
 
       last_print_ = now;
 
       std::stringstream ss;
 
-      ss << "{\"type\": \"instrumentation\", \"tag\": \"" << tag_
+      ss << "{\"type\": \"topic_delay\", \"tag\": \"" << tag_
          << "\", \"del\": [";
       
       for (duration & dt : *del) {
         ss << dt.count() << ',';
       }
 
-      ss <<"null], \"rtt\": [";
-
-      for (duration & dt : *rtt) {
-        ss << dt.count() << ',';
-      }
-         
       ss <<  "null]}" << std::endl;
 
       std::cout << ss.str();
@@ -73,11 +57,9 @@ public:
 private:
   std::string tag_;
 
-  time_point last_tick_{time_point::min()};
   time_point last_print_{time_point::min()};
 
   Counter delays_;
-  Counter rtts_;
 
   const duration PRINT_INTERVAL = 1s;
 };
