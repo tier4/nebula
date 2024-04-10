38072d4f8a8c930b02fd942c73dd1f2615c3c42e
Max SCHMELLER <max.schmeller@tier4.jp>
2024-04-10 12:23:36 +0900

    attempt to make queue contention less bad

--- nebula_ros/include/nebula_ros/hesai/mt_queue.hpp
+++ nebula_ros/include/nebula_ros/hesai/mt_queue.hpp
@@ -9,7 +9,7 @@ class mt_queue
 {
 private:
   std::mutex mutex_;
-  std::condition_variable condition_variable_;
+  std::condition_variable cv_not_empty_, cv_not_full_;
   std::deque<T> queue_;
   size_t capacity_;
 
@@ -18,34 +18,39 @@ public:
 
   bool try_push(T && value)
   {
-    {
-      std::unique_lock<std::mutex> lock(this->mutex_);
-      if (queue_.size() == capacity_) {
-        return false;
-      }
-
+    std::unique_lock<std::mutex> lock(this->mutex_);
+    bool can_push = queue_.size() < capacity_;
+    if (can_push) {
       queue_.push_front(std::move(value));
     }
-    this->condition_variable_.notify_one();
-    return true;
+    this->cv_not_empty_.notify_all();
+    return can_push;
   }
 
   void push(T && value)
   {
-    {
-      std::unique_lock<std::mutex> lock(this->mutex_);
-      this->condition_variable_.wait(lock, [=] { return this->queue_.size() < this->capacity_; });
-      queue_.push_front(std::move(value));
-    }
-    this->condition_variable_.notify_one();
+    std::unique_lock<std::mutex> lock(this->mutex_);
+    this->cv_not_full_.wait(lock, [=] { return this->queue_.size() < this->capacity_; });
+    queue_.push_front(std::move(value));
+    this->cv_not_empty_.notify_all();
   }
 
   T pop()
   {
     std::unique_lock<std::mutex> lock(this->mutex_);
-    this->condition_variable_.wait(lock, [=] { return !this->queue_.empty(); });
+    this->cv_not_empty_.wait(lock, [=] { return !this->queue_.empty(); });
     T return_value(std::move(this->queue_.back()));
     this->queue_.pop_back();
+    this->cv_not_full_.notify_all();
+
     return return_value;
+  }
+
+  void pop_all(std::deque<T> & dest) {
+    std::unique_lock<std::mutex> lock(this->mutex_);
+    this->cv_not_empty_.wait(lock, [=] { return !this->queue_.empty(); });
+    dest.insert(dest.end(), std::make_move_iterator(queue_.begin()), std::make_move_iterator(queue_.end()));
+    this->queue_.clear();
+    this->cv_not_full_.notify_all();
   }
 };
