0a30b9799d3a6d7957f6892e33a2f37013f41afa
Max SCHMELLER <max.schmeller@tier4.jp>
2024-06-26 19:52:01 +0900

    feat(aeva): add sensor model and type support

--- nebula_common/include/nebula_common/nebula_common.hpp
+++ nebula_common/include/nebula_common/nebula_common.hpp
@@ -343,7 +343,8 @@ enum class SensorModel {
   ROBOSENSE_BPEARL_V3,
   ROBOSENSE_BPEARL_V4,
   CONTINENTAL_ARS548,
-  CONTINENTAL_SRR520
+  CONTINENTAL_SRR520,
+  AEVA_AERIES2
 };
 
 /// @brief not used?
@@ -442,6 +443,9 @@ inline std::ostream & operator<<(std::ostream & os, nebula::drivers::SensorModel
       break;
     case SensorModel::CONTINENTAL_SRR520:
       os << "SRR520";
+      break;
+    case SensorModel::AEVA_AERIES2:
+      os << "Aeries II";
       break;
     case SensorModel::UNKNOWN:
       os << "Sensor Unknown";
@@ -581,6 +585,7 @@ inline SensorModel SensorModelFromString(const std::string & sensor_model)
   // Continental
   if (sensor_model == "ARS548") return SensorModel::CONTINENTAL_ARS548;
   if (sensor_model == "SRR520") return SensorModel::CONTINENTAL_SRR520;
+  if (sensor_model == "Aeries2") return SensorModel::AEVA_AERIES2;
   return SensorModel::UNKNOWN;
 }
 
@@ -631,6 +636,8 @@ inline std::string SensorModelToString(const SensorModel & sensor_model)
       return "ARS548";
     case SensorModel::CONTINENTAL_SRR520:
       return "SRR520";
+    case SensorModel::AEVA_AERIES2:
+      return "Aeries II";
     default:
       return "UNKNOWN";
   }
