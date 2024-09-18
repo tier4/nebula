dbd82621372066913ff534ed1c167e2e86d43ddd
Max SCHMELLER <max.schmeller@tier4.jp>
2024-09-18 15:38:08 +0900

    chore(hesai_ros_wrapper): add missing nebula_packet include

--- nebula_ros/include/nebula_ros/hesai/hesai_ros_wrapper.hpp
+++ nebula_ros/include/nebula_ros/hesai/hesai_ros_wrapper.hpp
@@ -26,6 +26,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
 
+#include <nebula_msgs/msg/nebula_packet.hpp>
 #include <pandar_msgs/msg/pandar_scan.hpp>
 
 #include <boost/algorithm/string/join.hpp>
