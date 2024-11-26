73228ef6d13d285a0da83a3eaec54dae9e4f4ff3
Max SCHMELLER <max.schmeller@tier4.jp>
2024-11-26 18:02:50 +0900

    chore(expected): make move-constructible
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- nebula_common/include/nebula_common/util/expected.hpp
+++ nebula_common/include/nebula_common/util/expected.hpp
@@ -15,6 +15,7 @@
 #pragma once
 
 #include <exception>
+#include <stdexcept>
 #include <string>
 #include <variant>
 
@@ -97,9 +98,10 @@ struct expected
     return default_;
   }
 
-  expected(const T & value) { value_ = value; }  // NOLINT(runtime/explicit)
-
-  expected(const E & error) { value_ = error; }  // NOLINT(runtime/explicit)
+  expected(const T & value) : value_(value) {}  // NOLINT(runtime/explicit)
+  expected(T && value) : value_(value) {}  // NOLINT(runtime/explicit)
+  expected(const E & error) : value_(error) {}  // NOLINT(runtime/explicit)
+  expected(E && error) : value_(error) {}  // NOLINT(runtime/explicit)
 
 private:
   std::variant<T, E> value_;
