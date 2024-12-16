b248dee7f0d7112468a291e7f80a500a38e087c9
Max SCHMELLER <max.schmeller@tier4.jp>
2024-12-16 10:12:39 +0900

    test(aeva): fix wrongly compiledand thus skipped unit test
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- nebula_tests/aeva/aeva_test_main.cpp
+++ nebula_tests/aeva/aeva_test_main.cpp
@@ -1,18 +1,49 @@
 // Copyright 2024 TIER IV, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
+#include "../common/mock_byte_stream.hpp"
+#include "../data/aeva/tcp_stream.hpp"
+#include "nebula_hw_interfaces/nebula_hw_interfaces_aeva/connections/pointcloud.hpp"
+
+#include <nebula_common/aeva/config_types.hpp>
+#include <nebula_hw_interfaces/nebula_hw_interfaces_aeva/connections/aeva_api.hpp>
 
 #include <gtest/gtest.h>
+
+#include <atomic>
+#include <memory>
+#include <thread>
+
+using nebula::drivers::aeva::PointCloudMessage;
+using nebula::drivers::connections::aeva::PointcloudParser;
+
+TEST(TestParsing, Pointcloud)  // NOLINT
+{
+  using std::chrono_literals::operator""ms;
+
+  auto mock_byte_stream = std::make_shared<nebula::test::MockByteStream>(STREAM);
+  PointcloudParser parser(mock_byte_stream);
+  std::atomic_bool done = false;
+
+  PointcloudParser::callback_t callback = [&](const PointCloudMessage & arg) {
+    if (done) return;
+
+    EXPECT_EQ(arg.header.aeva_marker, 0xAE5Au);
+    EXPECT_EQ(arg.header.platform, 2);
+
+    EXPECT_GT(arg.points.size(), 0);
+    EXPECT_TRUE(mock_byte_stream->done());
+    EXPECT_EQ(mock_byte_stream->get_read_count(), 2);
+    done = true;
+  };
+
+  parser.register_callback(std::move(callback));
+
+  mock_byte_stream->run();
+  while (!done) {
+    std::this_thread::yield();
+  }
+}
+
 
 int main(int argc, char * argv[])
 {
