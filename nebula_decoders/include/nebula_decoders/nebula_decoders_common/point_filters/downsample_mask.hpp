219b789857f3f8150475de284d1278d91547efaf
Max SCHMELLER <max.schmeller@tier4.jp>
2025-01-15 16:03:45 +0900

    chore(downsample_mask): add downsample_mask filter class
    The filter takes a path to a PNG image (will be converted to grayscale) and expected resolution/channel count of the mask.
    
    The grayscale mask is then dithered into a black/white mask of the same dimensions, and an `excluded(NebulaPoint const&)` function is provided to test whether a point shall be excluded according to the mask.
    
    For debug purposes, the dithered mask is written to the same directory as the input mask, with the file ending changed to `_dithered.png`. If this fails, the filter will not throw but log a warning.
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ nebula_decoders/include/nebula_decoders/nebula_decoders_common/point_filters/downsample_mask.hpp
@@ -0,0 +1,142 @@
+// Copyright 2024 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "nebula_decoders/nebula_decoders_common/angles.hpp"
+
+#include <nebula_common/loggers/logger.hpp>
+#include <nebula_common/nebula_common.hpp>
+#include <nebula_common/point_types.hpp>
+#include <nebula_common/util/string_conversions.hpp>
+#include <png++/error.hpp>
+#include <png++/gray_pixel.hpp>
+#include <png++/image.hpp>
+
+#include <Eigen/src/Core/Matrix.h>
+#include <sys/types.h>
+
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+namespace nebula::drivers::point_filters
+{
+
+namespace impl
+{
+
+inline void dither(
+  const png::image<png::gray_pixel> & in, png::image<png::gray_pixel> & out,
+  uint8_t quantization_levels)
+{
+  if (in.get_width() != out.get_width() || in.get_height() != out.get_height()) {
+    std::stringstream ss;
+    ss << "Expected downsample mask of size "
+       << "(" << out.get_width() << ", " << out.get_height() << ")";
+    ss << ", got "
+       << "(" << in.get_width() << ", " << in.get_height() << ")";
+
+    throw std::runtime_error(ss.str());
+  }
+
+  uint32_t denominator = quantization_levels;
+
+  auto should_keep = [denominator](uint32_t numerator, uint32_t pos) {
+    for (uint32_t i = 0; i < numerator; ++i) {
+      size_t dithered_pos = std::round(denominator / static_cast<double>(numerator) * i);
+      if (dithered_pos == pos) return true;
+    }
+    return false;
+  };
+
+  for (size_t y = 0; y < out.get_height(); ++y) {
+    for (size_t x = 0; x < out.get_width(); ++x) {
+      const auto & pixel = in.get_pixel(x, y);
+      uint32_t numerator = static_cast<uint32_t>(pixel) * denominator / 255;
+      size_t pos = (x + y) % denominator;
+      bool keep = should_keep(numerator, pos);
+      out.set_pixel(x, y, keep * 255);
+    }
+  }
+}
+
+}  // namespace impl
+
+class DownsampleMaskFilter
+{
+  static const uint8_t g_quantization_levels = 10;
+
+public:
+  DownsampleMaskFilter(
+    const std::string_view & filename, AngleRange<int32_t> azimuth_range_mdeg,
+    int32_t azimuth_peak_resolution_mdeg, size_t n_channels,
+    const std::shared_ptr<loggers::Logger> & logger)
+  : azimuth_range_{deg2rad(azimuth_range_mdeg.min / 1000.), deg2rad(azimuth_range_mdeg.max / 1000.)}
+  {
+    png::image<png::gray_pixel> factors(filename.data());
+
+    size_t mask_cols = azimuth_range_mdeg.extent() / azimuth_peak_resolution_mdeg;
+    size_t mask_rows = n_channels;
+
+    png::image<png::gray_pixel> dithered(mask_cols, mask_rows);
+    impl::dither(factors, dithered, g_quantization_levels);
+
+    mask_ = Eigen::MatrixX<uint8_t>(mask_rows, mask_cols);
+
+    for (size_t y = 0; y < dithered.get_height(); ++y) {
+      for (size_t x = 0; x < dithered.get_width(); ++x) {
+        mask_.coeffRef(y, x) = dithered.get_pixel(x, y);
+      }
+    }
+
+    std::string out_filename{filename};
+    size_t ext_pos = out_filename.find_last_of('.');
+    if (ext_pos != std::string::npos) {
+      out_filename = out_filename.substr(0, ext_pos);
+    }
+    out_filename += "_dithered.png";
+
+    try {
+      dithered.write(out_filename);
+      logger->info("Wrote dithered mask to " + out_filename);
+    } catch (const png::std_error & e) {
+      logger->warn("Could not write " + out_filename + ": " + e.what());
+    }
+  }
+
+  bool excluded(const NebulaPoint & point)
+  {
+    double azi_normalized = (point.azimuth - azimuth_range_.min) / azimuth_range_.extent();
+
+    auto x = static_cast<ssize_t>(azi_normalized * mask_.cols());
+    auto y = point.channel;
+
+    bool x_out_of_bounds = x < 0 || x >= mask_.cols();
+    bool y_out_of_bounds = y < 0 || y >= mask_.rows();
+
+    return x_out_of_bounds || y_out_of_bounds || !mask_.coeff(y, x);
+  }
+
+private:
+  AngleRange<double> azimuth_range_;
+  Eigen::MatrixX<uint8_t> mask_;
+};
+
+}  // namespace nebula::drivers::point_filters
