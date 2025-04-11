a9727b77baf931fbbdd57ec8f0fa980d9d857b9a
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-11 13:48:37 +0900

    feat(robosense): change to Nebula logging in HW interface
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- nebula_hw_interfaces/src/nebula_robosense_hw_interfaces/robosense_hw_interface.cpp
+++ nebula_hw_interfaces/src/nebula_robosense_hw_interfaces/robosense_hw_interface.cpp
@@ -2,17 +2,20 @@
 
 #include "nebula_hw_interfaces/nebula_hw_interfaces_robosense/robosense_hw_interface.hpp"
 
+#include "nebula_common/util/string_conversions.hpp"
+
 #include <iostream>
 #include <memory>
 #include <string>
 #include <utility>
 namespace nebula::drivers
 {
-RobosenseHwInterface::RobosenseHwInterface()
+RobosenseHwInterface::RobosenseHwInterface(const std::shared_ptr<loggers::Logger> & logger)
 : cloud_io_context_(new ::drivers::common::IoContext(1)),
   info_io_context_(new ::drivers::common::IoContext(1)),
   cloud_udp_driver_(new ::drivers::udp_driver::UdpDriver(*cloud_io_context_)),
-  info_udp_driver_(new ::drivers::udp_driver::UdpDriver(*info_io_context_))
+  info_udp_driver_(new ::drivers::udp_driver::UdpDriver(*info_io_context_)),
+  logger_(logger)
 {
 }
 
@@ -37,7 +40,9 @@ void RobosenseHwInterface::receive_info_packet_callback(std::vector<uint8_t> & b
 Status RobosenseHwInterface::sensor_interface_start()
 {
   try {
-    std::cout << "Starting UDP server for data packets on: " << *sensor_configuration_ << std::endl;
+    logger_->info(
+      "Starting UDP server for data packets on: " + sensor_configuration_->sensor_ip + ": " +
+      std::to_string(sensor_configuration_->data_port));
     cloud_udp_driver_->init_receiver(
       sensor_configuration_->host_ip, sensor_configuration_->data_port);
     cloud_udp_driver_->receiver()->open();
@@ -48,8 +53,9 @@ Status RobosenseHwInterface::sensor_interface_start()
         &RobosenseHwInterface::receive_sensor_packet_callback, this, std::placeholders::_1));
   } catch (const std::exception & ex) {
     Status status = Status::UDP_CONNECTION_ERROR;
-    std::cerr << status << sensor_configuration_->sensor_ip << ","
-              << sensor_configuration_->data_port << std::endl;
+    logger_->error(
+      util::to_string(status) + " " + sensor_configuration_->sensor_ip + "," +
+      std::to_string(sensor_configuration_->data_port));
     return status;
   }
   return Status::OK;
@@ -58,8 +64,7 @@ Status RobosenseHwInterface::sensor_interface_start()
 Status RobosenseHwInterface::info_interface_start()
 {
   try {
-    std::cout << "Starting UDP server for info packets on: " << *sensor_configuration_ << std::endl;
-    print_info(
+    logger_->info(
       "Starting UDP server for info packets on: " + sensor_configuration_->sensor_ip + ": " +
       std::to_string(sensor_configuration_->gnss_port));
     info_udp_driver_->init_receiver(
@@ -71,8 +76,9 @@ Status RobosenseHwInterface::info_interface_start()
       std::bind(&RobosenseHwInterface::receive_info_packet_callback, this, std::placeholders::_1));
   } catch (const std::exception & ex) {
     Status status = Status::UDP_CONNECTION_ERROR;
-    std::cerr << status << sensor_configuration_->sensor_ip << ","
-              << sensor_configuration_->gnss_port << std::endl;
+    logger_->error(
+      util::to_string(status) + " " + sensor_configuration_->sensor_ip + "," +
+      std::to_string(sensor_configuration_->gnss_port));
     return status;
   }
 
@@ -106,28 +112,4 @@ Status RobosenseHwInterface::register_info_callback(
   info_reception_callback_ = std::move(info_callback);
   return Status::OK;
 }
-
-void RobosenseHwInterface::print_debug(std::string debug)
-{
-  if (parent_node_logger_) {
-    RCLCPP_DEBUG_STREAM((*parent_node_logger_), debug);
-  } else {
-    std::cout << debug << std::endl;
-  }
-}
-
-void RobosenseHwInterface::print_info(std::string info)
-{
-  if (parent_node_logger_) {
-    RCLCPP_INFO_STREAM((*parent_node_logger_), info);
-  } else {
-    std::cout << info << std::endl;
-  }
-}
-
-void RobosenseHwInterface::set_logger(std::shared_ptr<rclcpp::Logger> logger)
-{
-  parent_node_logger_ = logger;
-}
-
 }  // namespace nebula::drivers
