ce9541f514a6f386e60099f5e3f75185bd172a34
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-29 20:07:19 +0200

    chore(hesai): add CRC checks for OT128 and QT128
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- nebula_decoders/include/nebula_decoders/nebula_decoders_hesai/decoders/pandar_128e3x.hpp
+++ nebula_decoders/include/nebula_decoders/nebula_decoders_hesai/decoders/pandar_128e3x.hpp
@@ -17,6 +17,10 @@
 #include "nebula_decoders/nebula_decoders_hesai/decoders/hesai_packet.hpp"
 #include "nebula_decoders/nebula_decoders_hesai/decoders/hesai_sensor.hpp"
 
+#include <nebula_common/util/crc.hpp>
+
+#include <iostream>
+#include <ostream>
 #include <vector>
 
 namespace nebula::drivers
@@ -40,20 +44,20 @@ struct Tail128E3X
 
   /* Ignored optional fields */
 
-  // uint32_t udp_sequence;
+  uint32_t udp_sequence;
 
-  // uint16_t imu_temperature;
-  // uint16_t imu_acceleration_unit;
-  // uint16_t imu_angular_velocity_unit;
-  // uint32_t imu_timestamp;
-  // uint16_t imu_x_axis_acceleration;
-  // uint16_t imu_y_axis_acceleration;
-  // uint16_t imu_z_axis_acceleration;
-  // uint16_t imu_x_axis_angular_velocity;
-  // uint16_t imu_y_axis_angular_velocity;
-  // uint16_t imu_z_axis_angular_velocity;
+  uint16_t imu_temperature;
+  uint16_t imu_acceleration_unit;
+  uint16_t imu_angular_velocity_unit;
+  uint32_t imu_timestamp;
+  uint16_t imu_x_axis_acceleration;
+  uint16_t imu_y_axis_acceleration;
+  uint16_t imu_z_axis_acceleration;
+  uint16_t imu_x_axis_angular_velocity;
+  uint16_t imu_y_axis_angular_velocity;
+  uint16_t imu_z_axis_angular_velocity;
 
-  // uint32_t crc_tail;
+  uint32_t crc_tail;
 
   /// @brief Get the azimuth state of the given block in the packet
   /// @param block_id The block ID (i.e. its index in the packet). Valid IDs are 0 and 1.
@@ -62,15 +66,42 @@ struct Tail128E3X
   {
     return (azimuth_state >> (14 - block_id * 2)) & 0b11;
   }
+
+  [[nodiscard]] bool is_crc_valid() const
+  {
+    return crc<crc32_mpeg2_t>(&reserved1, &crc_tail) == crc_tail;
+  }
+};
+
+struct ChannelHealth128E3X
+{
+  uint16_t fault_info_num;
+  uint32_t channel_health_status;
+  uint8_t reserved[2];
+};
+
+struct FunctionalSafety128E3X
+{
+  uint8_t fs_version;
+  uint8_t lidar_state;
+  uint8_t fault_code_id;
+  uint16_t fault_code;
+  ChannelHealth128E3X channel_health;
+  uint32_t crc_fs;
+
+  [[nodiscard]] bool is_crc_valid() const
+  {
+    // fs_version is not included in the CRC check
+    return crc<crc32_mpeg2_t>(&lidar_state, &crc_fs) == crc_fs;
+  }
 };
 
 struct Packet128E3X : public PacketBase<2, 128, 2, 100>
 {
-  using body_t = Body<Block<Unit3B, Packet128E3X::n_channels>, Packet128E3X::n_blocks>;
+  using body_t = BodyWithCrc<Block<Unit3B, Packet128E3X::n_channels>, Packet128E3X::n_blocks>;
   Header12B header;
   body_t body;
-  uint32_t crc_body;
-  FunctionalSafety fs;
+  FunctionalSafety128E3X fs;
   Tail128E3X tail;
 
   /* Ignored optional fields */
