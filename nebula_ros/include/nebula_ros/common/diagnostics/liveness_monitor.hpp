c604786f52cb77a48950b8696663a37825961e10
Max SCHMELLER <max.schmeller@tier4.jp>
2025-05-16 16:47:17 +0900

    refactor: move liveness monitor to own directory
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- nebula_ros/include/nebula_ros/common/diagnostics/liveness_monitor.hpp
+++ nebula_ros/include/nebula_ros/common/diagnostics/liveness_monitor.hpp
@@ -16,12 +16,17 @@
 
 #include <diagnostic_updater/diagnostic_status_wrapper.hpp>
 #include <diagnostic_updater/diagnostic_updater.hpp>
+#include <rclcpp/duration.hpp>
+#include <rclcpp/node.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
 
 #include <diagnostic_msgs/msg/detail/diagnostic_status__struct.hpp>
 
 #include <chrono>
+#include <cstdint>
 #include <string>
+#include <utility>
 
 namespace nebula::ros
 {
@@ -46,9 +51,9 @@ public:
    * @param timeout The time after the last call to `tick()` where liveliness is
    */
   LivenessMonitor(
-    rclcpp::Node & node, diagnostic_updater::Updater & diagnostic_updater, const std::string & name,
-    const std::chrono::nanoseconds & timeout)
-  : timer_(node.create_wall_timer(timeout, [this] { on_timeout(); }))
+    rclcpp::Node::SharedPtr node, diagnostic_updater::Updater & diagnostic_updater,
+    const std::string & name, const rclcpp::Duration & timeout)
+  : timeout_(timeout), node_(std::move(node))
   {
     diagnostic_updater.add(name, [this](diagnostic_updater::DiagnosticStatusWrapper & status) {
       report_liveness(status);
@@ -61,26 +66,36 @@ public:
    * A call to `tick()` resets the internal timer. If the timer is not reset at least once before it
    * expires (within `timeout`), the monitored routine is pronounced dead.
    */
-  void tick() { timer_->reset(); }
+  void tick() { last_tick_ = node_->get_clock()->now(); }
 
 private:
-  void on_timeout()
-  {
-    is_live_ = false;
-    diagnostic_updater_->force_update();
-  }
-
   void report_liveness(diagnostic_updater::DiagnosticStatusWrapper & status) const
   {
     using diagnostic_msgs::msg::DiagnosticStatus;
 
-    status.summary(is_live_ ? DiagnosticStatus::OK : DiagnosticStatus::ERROR, "");
-    status.add("alive", is_live_ ? "true" : "false");
+    rclcpp::Time now = node_->get_clock()->now();
+    rclcpp::Duration lateness = now - last_tick_;
+    bool is_live = lateness < timeout_;
+
+    uint8_t severity = DiagnosticStatus::OK;
+    std::string message = "Alive";
+    std::string value = "true";
+
+    if (!is_live) {
+      severity = DiagnosticStatus::ERROR;
+      message = "Dead";
+      value = "false";
+    }
+
+    status.summary(severity, message);
+    status.add("is_alive", value);
+    status.add("last_tick", last_tick_.seconds());
+    status.add("lateness", lateness.seconds());
   }
 
-  rclcpp::TimerBase::SharedPtr timer_;
-  diagnostic_updater::Updater * diagnostic_updater_;
-  bool is_live_{false};
+  rclcpp::Time last_tick_;
+  rclcpp::Duration timeout_;
+  rclcpp::Node::SharedPtr node_;
 };
 
 }  // namespace nebula::ros
