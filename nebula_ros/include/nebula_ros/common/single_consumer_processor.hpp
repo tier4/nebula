40d08c7044860e44f0089a32cc7c75d9f9d68bef
Max Schmeller <6088931+mojomex@users.noreply.github.com>
2025-07-15 10:28:09 +0900

    perf(hesai): publish packets in separate thread (#343)
    * chore(nebula_ros): add new queue-based threaded consumer class
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * perf: move packet publishing to own thread
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * perf(hesai/decoder_wrapper): disable checking for packets subscribers due to interference with packets publish thread
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * chore: update comment regarding packet subscription check
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * chore: remove double-join, notify producer on stop
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * fix: fix try_push failing on  non-full queue when queue operation is in progress
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * chore: finish processing queued items on stop()
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * chore: add thread safety annotations
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * chore: add is_stopping function to facilitate testing
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    * test: add unit tests for single consumer processor
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>
    
    ---------
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ nebula_ros/include/nebula_ros/common/single_consumer_processor.hpp
@@ -0,0 +1,173 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <rcpputils/thread_safety_annotations.hpp>
+
+#include <cassert>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <utility>
+
+namespace nebula::ros
+{
+
+/// @brief A thread-safe single-consumer queue processor that runs a callback function
+/// on items in a separate thread. The queue has a maximum size, so insertions block or
+/// are rejected if the consumer is slower than the producer.
+/// @tparam T The type of items to process
+template <typename T>
+class SingleConsumerProcessor
+{
+public:
+  using callback_t = std::function<void(T &&)>;
+
+  /// @brief Constructor
+  /// @param callback The callback function to execute on each item
+  /// @param max_queue_size The maximum size of the queue
+  explicit SingleConsumerProcessor(callback_t callback, size_t max_queue_size)
+  : callback_(std::move(callback)), max_queue_size_(max_queue_size)
+  {
+    if (max_queue_size == 0) {
+      throw std::invalid_argument("Max queue size must be greater than 0");
+    }
+
+    if (!callback_) {
+      throw std::invalid_argument("Callback function must not be null");
+    }
+
+    consumer_thread_ = std::thread(&SingleConsumerProcessor::consumer_loop, this);
+  }
+
+  /// @brief Destructor - stops the processing thread
+  ~SingleConsumerProcessor() { stop(); }
+
+  // Delete copy and move constructors/operators to prevent accidental copying
+  SingleConsumerProcessor(const SingleConsumerProcessor &) = delete;
+  SingleConsumerProcessor & operator=(const SingleConsumerProcessor &) = delete;
+  SingleConsumerProcessor(SingleConsumerProcessor &&) = default;
+  SingleConsumerProcessor & operator=(SingleConsumerProcessor &&) = default;
+
+  /// @brief Add an item to the queue for processing. Blocks until the item has been queued.
+  /// @param item The item to process
+  void push(T && item)
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex_);
+
+    cv_can_push_.wait(lock, [this]() { return should_stop_ || (queue_.size() < max_queue_size_); });
+
+    if (should_stop_) {
+      return;
+    }
+
+    queue_.push(std::move(item));
+    lock.unlock();
+    cv_can_pop_.notify_one();
+  }
+
+  /// @brief Try to add an item to the queue for processing without blocking.
+  /// @param item The item to process
+  /// @return True if the item was added, false if the queue is full
+  bool try_push(T && item)
+  {
+    std::unique_lock lock(queue_mutex_);
+
+    if (should_stop_) {
+      return false;
+    }
+
+    if (queue_.size() >= max_queue_size_) {
+      return false;
+    }
+
+    queue_.push(std::move(item));
+    lock.unlock();
+    cv_can_pop_.notify_one();
+    return true;
+  }
+
+  /// @brief Stop the consumer thread
+  void stop()
+  {
+    {
+      std::lock_guard lock(queue_mutex_);
+      should_stop_ = true;
+    }
+
+    cv_can_pop_.notify_all();
+    cv_can_push_.notify_all();
+
+    if (consumer_thread_.joinable()) {
+      consumer_thread_.join();
+    }
+  }
+
+  /// @brief Check if the processor is in stopping state
+  /// @return True if stop() has been called, false otherwise
+  bool is_stopping() const
+  {
+    std::lock_guard lock(queue_mutex_);
+    return should_stop_;
+  }
+
+private:
+  void consumer_loop()
+  {
+    std::unique_lock lock(queue_mutex_, std::defer_lock);
+
+    while (true) {
+      lock.lock();
+      // Wait for an item to be available or for stop signal
+      cv_can_pop_.wait(lock, [this] { return !queue_.empty() || should_stop_; });
+
+      if (should_stop_) {
+        break;
+      }
+
+      T item = std::move(queue_.front());
+      queue_.pop();
+      lock.unlock();
+      cv_can_push_.notify_one();
+      callback_(std::move(item));
+    }
+
+    assert(lock.owns_lock());
+
+    // Process the remaining items when stopping.
+    // No need to unlock as the consumer
+    while (!queue_.empty()) {
+      T item = std::move(queue_.front());
+      queue_.pop();
+      callback_(std::move(item));
+    }
+  }
+
+  callback_t callback_;
+  std::thread consumer_thread_;
+
+  mutable std::mutex queue_mutex_;
+  std::condition_variable cv_can_pop_;
+  std::condition_variable cv_can_push_;
+
+  size_t max_queue_size_;
+  std::queue<T> queue_ {}
+  RCPPUTILS_TSA_GUARDED_BY(queue_mutex_);
+  bool should_stop_{false} RCPPUTILS_TSA_GUARDED_BY(queue_mutex_);
+};
+
+}  // namespace nebula::ros
